2ed3eb1e4a6eeaf3557f5cfbd3109d7404d17763
Mathieu Le Bihan <mlebihan22@gmail.com>
2017-10-19 21:49:48 +0200

    fix applying of nodes style

--- example/src/App.js
+++ example/src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 export default class App extends Component {
   render () {
     return (
-      <div>
+      <div className="App">
         <Demo
           background={'none'}
           target={Tree}
